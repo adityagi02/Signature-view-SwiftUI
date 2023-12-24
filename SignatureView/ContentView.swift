1356ce2bbeed55c12dfbe617396af4d52d9b344a
adityagi02 <adityagi02@yahoo.com>
2023-12-24 16:32:29 +0530

    Initial Commit

---
+++ SignatureView/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SignatureView
+//
+//  Created by Aditya Tyagi on 24/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SignatureView/SignatureViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SignatureViewApp.swift
+//  SignatureView
+//
+//  Created by Aditya Tyagi on 24/12/23.
+//
+
+import SwiftUI
+
+@main
+struct SignatureViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
